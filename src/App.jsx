3a5f85d50ac7ddffab7c094ef35f063cd523bfbc
Ritaakterlaboni <ritaakterlaboni4@gmail.com>
2025-08-31 11:04:02 +0600

    update effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 
 import { useState } from 'react';
 import './App.css'
+import Snowfall from "react-snowfall";
+
 import { MdKeyboardDoubleArrowRight } from "react-icons/md";
 
 function App() {
@@ -9,18 +11,27 @@ const [open,setOpen] = useState(false);
 const [hide, setHide] = useState(false);
 
 
+
+
+
   return (
    <>
 <div className=" ">
-     <div className="relative h-screen w-full bg-gradient-to-t from-purple-600 to-blue-600">
+  <div className="relative h-screen w-full bg-gradient-to-t from-purple-600 to-blue-600">
+   
+   {/* snowfalleffect dite npm react thake ai tag ta dile import kore tahole showfall hobe */}
+   
+    <Snowfall color="white" snowflakeCount={100} />         
+
+{/* react snowfall end */}
      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-xl bg-slate-300 sm:w-[500px] sm:h-[450px] w-[450px] h-[510px] p-[20px] border-transparent border rounded-lg">
 
       <h1 className="text-2xl font-bold mb-4">Frequently Asked Questions (FAQ)</h1>
       
        {/* first question */}
-    <div className="" onClick={() => setShow(!show)}>1. What technologies does this project use?
+    <div className="font-medium" onClick={() => setShow(!show)}>1. What technologies does this project use?
       {( show &&
-        <div className="">
+        <div className="font-normal text-[18px]">
         <p><MdKeyboardDoubleArrowRight/>This project is built using React.js for the frontend and Tailwind CSS for styling.</p>
       </div>
       )}
@@ -28,11 +39,13 @@ const [hide, setHide] = useState(false);
  {/* first question */}
 
   {/* second question */}
-    <div onClick={() => setOpen(!open)} className="">2. How can I customize the questions and answers?
+    <div onClick={() => setOpen(!open)} className="font-medium">2. How can I customize the questions and answers?
     
     {( open &&
-     <p><MdKeyboardDoubleArrowRight/>You can customize the questions and answers by editing the faqs array in the code. Each entry has an id, question, and answer field.</p>
+    <div className="font-normal text-[18px]">
+       <p><MdKeyboardDoubleArrowRight/>You can customize the questions and answers by editing the faqs array in the code. Each entry has an id, question, and answer field.</p>
 
+    </div>
     )}
     
     </div>
@@ -40,20 +53,22 @@ const [hide, setHide] = useState(false);
      {/* second question */}
 
       {/* third question */}
-    <div onClick={() => setHide(!hide)} className="">3. Is this FAQ section mobile responsive?
+    <div onClick={() => setHide(!hide)} className="font-medium">3. Is this FAQ section mobile responsive?
     {( hide && 
-      <p> <MdKeyboardDoubleArrowRight />
-                  Yes, the FAQ section is fully responsive and works well on all screen sizes including mobile devices, thanks to Tailwind CSS.</p>
+     <div className="font-normal text-[18px]">
+       <p> <MdKeyboardDoubleArrowRight />Yes, the FAQ section is fully responsive and works well on all screen sizes including mobile devices, thanks to Tailwind CSS.</p>
+     </div>
     )}
 
     </div>
 
       {/* third question */}
      </div>
-   </div>
+  </div>
 
 
 </div>
+
    </>
   )
 }
